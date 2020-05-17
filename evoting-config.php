5c5c497eb354a4a5c3d561dcb020102d23948796
Ali Naufal Ammarullah <ali_naufal26@apps.ipb.ac.id>
2020-05-17 14:39:00 +0700

    update data berita

--- evoting-config.php
+++ evoting-config.php
@@ -1,5 +1,5 @@
 <?php
-    $db = pg_connect('host=localhost dbname=evoting user=postgres password=postgres');
+    $db = pg_connect('host=localhost dbname=evoting user=postgres password=admin');
     if( !$db ){
         die("Gagal terhubung dengan database: " . pg_connect_error());
     }
